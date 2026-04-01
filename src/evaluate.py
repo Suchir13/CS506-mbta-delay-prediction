a6d2f329ccfb178e2cb12ce454ad6477b124a348
ps2001-sys <ps2001@bu.edu>
2026-03-31 19:53:36 -0700

    import in evaluate

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,4 +1,12 @@
 import pandas as pd
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score,
+    average_precision_score
+)
 
 TEST_PATH = "data/processed/test_predictions.csv"
 VAL_PATH = "data/processed/val_predictions.csv"
